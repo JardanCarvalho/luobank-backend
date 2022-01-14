a4a2f075ba36b4ef12067ade0484a801c041487a
JardanCarvalho <jardan.carvalho@gmail.com>
2022-01-14 19:01:56 -0300

    mongoose installed, mongoDB connected

---
+++ db/index.ts
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect( 'mongodb+srv://luobank:LuoBank102030@luobank.zm0vv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+
+});
+
