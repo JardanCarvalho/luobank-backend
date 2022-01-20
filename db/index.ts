2dbe9ae444d34cc18a023aeddd4534e85998e8aa
Jardan Carvalho <jardan.carvalho@gmail.com>
2022-01-20 00:33:35 -0300

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -2,6 +2,4 @@ const mongoose = require('mongoose');
 
 mongoose.connect( 'mongodb+srv://luobank:LuoBank102030@luobank.zm0vv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
-
 });
-
